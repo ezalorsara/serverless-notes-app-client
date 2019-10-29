33831605454669f78d53c9eaae1526f8877c0341
Joed Parin <parinjoed@gmail.com>
2019-10-29 23:46:47 +0800

    feat: added router, react-bootstrap and home container

---
+++ src/Router.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './containers/Home';
+
+
+const Routes: React.FC = () => {
+  return (
+    <Switch>
+        <Route path="/" exact component={Home} />
+    </Switch>
+  );
+}
+
+export default Routes;
